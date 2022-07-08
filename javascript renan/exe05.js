dbd4b9dc1d38d97e29978104dd1bbd7002697d58
anaboteleiro <106985798+anaboteleiro@users.noreply.github.com>
2022-07-08 17:03:52 -0300

    Create exe05.js
    Media aluno

---
+++ javascript renan/exe05.js
@@ -0,0 +1,13 @@
+// Faça um script que some duas notas de um aluno usando estrutura condicional (IF)
+var media = 80 
+    var nota1 = 10;
+    var nota2 = 7.5;
+
+    var media = (nota1 + nota2)/2 ;
+
+    if(media >= 7){console.log("Aprovado");}
+      
+      else{
+     console.log("Reprovado!")}
+
+   
