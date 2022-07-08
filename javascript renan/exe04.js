6cf640401f0c2b4e7c9d0d7310c82d6039ddc35f
anaboteleiro <106985798+anaboteleiro@users.noreply.github.com>
2022-07-08 16:41:50 -0300

    Create exe04.js
    numero em um intervalo

---
+++ javascript renan/exe04.js
@@ -0,0 +1,18 @@
+/**
+ * Crie um script usando estrutura condicional (IF...ELSE) que deve verificar:
+ * - se o valor da variável {numero} está entre o valor da variável {inicio} e o valor da variavel {final}
+ * - caso esteja dentro do intervalo (maior ou igual a inicio E menor ou igual a final), exiba uma mensagem
+ *    que o número está dentro deste intervalo;
+ * - caso este número esteja fora do intervalo, exiba uma mensagem que o número está fora do intervalo;
+ */
+
+ let numero = 10;
+ let inicio;
+ let final;
+ 
+ // escreva seu código aqui
+ if (numero < 0) {
+    console.log("inicio");
+} else {
+    console.log("final");
+}
