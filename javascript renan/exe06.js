f9ac4e64e990f096cadf18fa089200019c3749f9
anaboteleiro <106985798+anaboteleiro@users.noreply.github.com>
2022-07-08 17:08:47 -0300

    Create exe06.js
    calculadora

---
+++ javascript renan/exe06.js
@@ -0,0 +1,8 @@
+let primeiroNumero = 10;
+let segundoNumero = 20;
+let operacao = "somar";
+
+if (operacao == "somar") {
+  const calculo = primeiroNumero + segundoNumero;
+  console.log(`Soma de ${primeiroNumero} e ${segundoNumero} é ${calculo}`);
+}
