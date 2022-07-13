a25544b4d434ab58b78166645fcdae346c40d927
Ana Boteleiro <106985798+anaboteleiro@users.noreply.github.com>
2022-07-13 14:23:20 -0300

    aula com Gabriel e Daniel

---
+++ java objetos/exobjetos01.js
@@ -0,0 +1,43 @@
+
+/*exerc.01 *\
+// const Carro = { marca:'volvo', modelo: 'Prarrico', cor:'pink', Ano:'2003'};
+// console.log(Carro)
+
+// console.log(Carro.Ano)
+
+
+/*exerc.02*\
+
+// const Aluno ={"Nome completo": 'Ana Lucia', matricula:39, turma:103 };
+// console.log(Aluno['Nome completo', 'matricula','turma']);
+// console.log(Aluno)
+// console.log( 'O '+ Aluno["Nome completo"] +" foi aprovada por ser cranio "+ "na "+ Aluno.turma)
+
+/*exerciio.03 e 04*\
+// const nome = window.prompt('Digite seu Nome');
+// console.log(nome);
+// const Idade = window.prompt('Digite sua Idade');
+// console.log(nome,Idade);
+
+
+/*exerci.05*\
+const casa = {cor:'maresia', "ano de construção":1983  };
+console.log(casa);
+casa.janelas= ('10');
+console.log(casa);
+console.log(casa["ano de construção"]+1);
+delete casa.cor
+console.log(casa)
+
+
+/*exerc.07*\
+const pessoa={ nome:'Ana Lucia' };
+const Idade = window.prompt('Digite sua Idade');
+pessoa.idade = Idade;
+if (
+    pessoa.idade >=60
+){
+    delete pessoa.idade
+}
+
+    console.log(pessoa)
