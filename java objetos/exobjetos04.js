a60f90c98b7ae0a3cc73941548e99bcb4f45e3c6
Ana Boteleiro <106985798+anaboteleiro@users.noreply.github.com>
2022-07-14 17:04:25 -0300

    Add files via upload

---
+++ java objetos/exobjetos04.js
@@ -0,0 +1,59 @@
+// //ex01 funçoes
+// function ola (nome){
+//     return 'olá mundo'
+// }
+// const retornodafunção = ola("mundo crel);
+// console.log (retornodafunção)
+
+
+
+
+
+
+// let multiplicacao = function (valor){
+//     console.log (valor*valor*valor)
+// }
+// multiplicacao(2)
+
+// const ola= function(nome){
+//     return 'ola ' + nome;
+// }
+
+//ex03
+// const saudação= function(){
+//     return 'olá mundo'
+// }
+// const retornodafunção = ola( ' ana');
+// console.log(retornodafunção)
+
+// function ola(nome){
+//     console.log('ola' + nome)
+// }
+
+// ola (' Ana Lucia');
+
+
+//ex04
+const cubo = function (numero) {
+    console.log( numero *numero)
+}
+
+cubo(5)
+cubo(2)
+cubo(1)
+
+
+//05
+function arearetangular(base,altura){
+    return base*altura
+}
+const area = arearetangular(4,10);
+console.log('A area do retangulo é '+ area)
+
+
+const olamundo =()=>{
+    console.log("ola mundo")
+}
+const olamundo =function =>{
+    console.log("ola mundo")
+}
